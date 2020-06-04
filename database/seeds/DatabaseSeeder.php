9a344251fedb165bfc310566ae0c24184314e02b
Karol Kosiacki <karol.kosiacki@inpay.pl>
2020-06-04 10:51:28 +0200

    remove vote seeders data (for clean test environment)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call(PollSeeder::class);
-        $this->call(VoteSeeder::class);
+        //$this->call(VoteSeeder::class);
     }
 }
